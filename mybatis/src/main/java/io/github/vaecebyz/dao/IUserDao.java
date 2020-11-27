415119fc191a465952d0ba393de9210d5466aaba
vaecebyZ <vaeceby2@qq.com>
2020-11-27 16:11:47 +0800

    改造成注解模式

--- mybatis/src/main/java/io/github/vaecebyz/dao/IUserDao.java
+++ mybatis/src/main/java/io/github/vaecebyz/dao/IUserDao.java
@@ -1,11 +1,15 @@
 package io.github.vaecebyz.dao;
 
 import io.github.vaecebyz.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IUserDao  {
 
     //查询所有
+    //使用注解
+    @Select("select * from people")
+
     List<User> findAll();
 }
--- mybatis/src/test/java/io/github/vaecebyz/test/MybatisTest.java
+++ mybatis/src/test/java/io/github/vaecebyz/test/MybatisTest.java
@@ -13,21 +13,21 @@ import java.util.List;
 public class MybatisTest {
 
     public static void main(String[] args) throws Exception {
-//            读取配置文件
+//读取配置文件
        InputStream in = Resources.getResourceAsStream("SqlMapConfig.xml");
-//        创建sessionFactory
+//创建sessionFactory
        SqlSessionFactoryBuilder builder = new SqlSessionFactoryBuilder();
        SqlSessionFactory factory =   builder.build(in);
-//        使用工厂生产对象
+//使用工厂生产对象
         SqlSession session =  factory.openSession();
-//        使用创建代理对象
+//使用创建代理对象
         IUserDao userDao = session.getMapper(IUserDao.class);
-//        使用代理对象执行方法
+//使用代理对象执行方法
         List<User> users = userDao.findAll();
         for (User user : users){
             System.out.println(user);
         }
-//        释放资源
+//释放资源
         session.close();
         in.close();
 
